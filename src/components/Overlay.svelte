0d406509e3c6baca0cdebba564b96c7c80dd2d97
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2023-04-13 09:10:51 +0200

    correct handling of keyup in overlays

--- src/components/Overlay.svelte
+++ src/components/Overlay.svelte
@@ -3,25 +3,12 @@
   import { createEventDispatcher } from "svelte";
   const dispatch = createEventDispatcher();
   import { toast } from "@zerodevx/svelte-toast";
-  import {
-    no_text_inputs_focused,
-    focus_first_in_node,
-  } from "$lib/util.ts";
+  import { no_text_inputs_focused, focus_first_in_node } from "$lib/util.ts";
 
   export let overlay = false;
   let last_overlay = "";
 
   onMount(async () => {});
-
-  function start_listening() {
-    console.log("start listening");
-    return "";
-  }
-
-  function stop_listening() {
-    console.log("stop overaly listen");
-    return "";
-  }
 
   onDestroy(async () => {});
 
@@ -36,12 +23,12 @@
   });
 
   function onblur() {
-	focus_first_in_node(document.getElementById("overlay"));
-	toast.push("onblur");
+    focus_first_in_node(document.getElementById("overlay"));
+    toast.push("onblur");
   }
 
   onMount(() => {
-	focus_first_in_node(document.getElementById("overlay"));
+    focus_first_in_node(document.getElementById("overlay"));
   });
 
   function handle_keyup(ev) {
@@ -56,10 +43,4 @@
 
 <div id="overlay" on:keyup={handle_keyup}>
   <slot />
-
-  {#if overlay != ""}
-    {start_listening()}
-  {:else}
-    {stop_listening()}
-  {/if}
 </div>
